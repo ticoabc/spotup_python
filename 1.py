b272f9024a24b67cf343bf0f90e8a05d3ba507b7
Tiago de Freitas <tiago.freitas2@fatec.sp.gov.br>
2022-10-06 18:03:43 -0300

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,20 @@
+#Exercício 1 - Soma de dois números inteiros
+#Autor: Tiago de Freias
+#Data atual: 29-Ago-22
+#01. Faça um algoritmo que receba 2 números inteiros e apresente a soma desses números.
+
+#Constantes e Variáveis
+A=0
+B=0
+R=0
+
+#Leitura de Variáveis
+print('Exercício 1 - Soma de dois números inteiros')
+A=int(input('Valor de A: '))
+B=int(input('Valor da B: '))
+
+#Cálculos
+R=A+B
+
+#Exibir Resultados 
+print('A: ', A, 'B: ', B, 'Soma: ', R)
